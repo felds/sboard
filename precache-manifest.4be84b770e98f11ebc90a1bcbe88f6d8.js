1355d3722f7a365dc5ede63c684779dcab853d1b
Felds Liscia <dev@felds.com.br>
2019-10-01 01:57:46 -0300

    Updates

---
+++ precache-manifest.4be84b770e98f11ebc90a1bcbe88f6d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef41da375fc0ce4bb7d9fd24d3580ba3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d09abaf0014a3374abb",
+    "url": "/static/css/main.0a6077ce.chunk.css"
+  },
+  {
+    "revision": "7e09f2a117a8995f3fed",
+    "url": "/static/js/2.140ab911.chunk.js"
+  },
+  {
+    "revision": "7d09abaf0014a3374abb",
+    "url": "/static/js/main.4fc89a70.chunk.js"
+  },
+  {
+    "revision": "ec2abc0cc8da36008515",
+    "url": "/static/js/runtime-main.ee34a309.js"
+  }
+]);
